2da0e9dc5343c8495835d5a86fcb266288b157af
BegaJulian <Julian.begamilrad@gmail.com>
2020-09-29 21:32:00 -0300

    Shape & Entity base

---
+++ DDG_engine/game engine/Entity.h
@@ -0,0 +1,20 @@
+
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include "export.h"
+
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+#include "glew.h"
+#include "glfw3.h"
+
+class ENGINE_API Entity {
+
+public:
+	Entity();
+	~Entity();
+
+};
+#endif // !WINDOW_H
---
+++ DDG_engine/game engine/Shape.h
@@ -0,0 +1,20 @@
+
+#ifndef SHAPE_H
+#define SHAPE_H
+
+#include "export.h"
+
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+#include "glew.h"
+#include "glfw3.h"
+
+class ENGINE_API Shape {
+
+public:
+	Shape();
+	~Shape();
+
+};
+#endif // !WINDOW_H
